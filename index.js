e8bffd554183d8dc4d25e2c466b20ca552b3bc32
Oxana Chiziţcaia <creativesouloxana@gmail.com>
2024-04-06 13:30:48 +0200

    Added formalGoodbye and rudeGoodbye

--- index.js
+++ index.js
@@ -6,9 +6,19 @@ const fillSubject = (subject) => {
   document.querySelector('.email__subject').textContent = subject;
 };
 
-const fillBody = (body, name) => {
+const fillBody = (body, name, goodbyeFunction) => {
   const bodyElm = document.querySelector('.email__body');
   bodyElm.innerHTML += body;
   const closingElm = document.querySelector('.email__closing');
-  closingElm.textContent = goodbye(name);
+  closingElm.textContent = goodbyeFunction(name);
 };
+
+const formalGoodbye = (name) => {
+  return 'S uctivou poklonou ' + name;
+};
+
+const rudeGoodbye = (name) => {
+  return 'Se měj ' + name;
+};
+
+fillBody('Zítra oslava. 18:00 ve Starý hospodě.', 'Patrik Veselý', rudeGoodbye);
